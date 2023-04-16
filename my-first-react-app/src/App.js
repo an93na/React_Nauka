bc3160751d30ca5806cb3a1e839b466eeb6e7b3a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-16 19:07:47 +0200

    changes

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -10,14 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <button>click</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
