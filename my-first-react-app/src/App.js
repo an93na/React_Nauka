deb563f7740502f30bbb5e2eb916db417933ebf6
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-16 19:34:25 +0200

    changes

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button>click</button>
-      </header>
-    </div>
+        <article>To jest artykuł</article>
   );
 }
 
