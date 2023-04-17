c2a0ff31eda57e7bd4355f0e9c8a9471b9efa883
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-17 17:20:10 +0200

    changes

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import '@picocss/pico'
-
+import "./App.css";
+import "@picocss/pico";
 
 function App() {
   return (
     <>
-    <header>
-      <article>To jest Test</article>
-    </header>
-    <article>To jest artykuł
-      <p>Jakiś paragraf</p>
-      <div>Jakiś div</div>
-    </article>
+      <header>
+        <article>To jest Test</article>
+      </header>
+      <article>
+        To jest artykuł
+        <p>Jakiś paragraf</p>
+        <div>Jakiś div</div>
+      </article>
     </>
   );
 }
