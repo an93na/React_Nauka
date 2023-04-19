aabd22be3ea5feb0fa5ecfd53aac5fbdf944143f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-19 06:45:11 +0200

    changes

--- homework/src/ex1/komponentFunkcyjny.js
+++ homework/src/ex1/komponentFunkcyjny.js
@@ -14,7 +14,7 @@ export const KomponentFunkcyjny = () => {
   };
 
   const tablica = [1, 2, 3, 4];
-  
+
   // funkcja wewnętrzna wewnątrz komponentu
   const addTwoNumbers = (a, b) => {
     return a + b;
@@ -27,6 +27,11 @@ export const KomponentFunkcyjny = () => {
       {notatki}
       {`Name: ${obiekt.name} Age: ${obiekt.age} Job: ${obiekt.job}`}
       <div>{tablica}</div>
+
+      <div>
+        <p>Wynik dodawania 5 + 10 = {addTwoNumbers(5,10)}</p>
+        <p>Wynik mnożenia 5 * 10 = {multiplyTwoNumbers(5,10)}</p>
+      </div>
     </article>
   );
 };
