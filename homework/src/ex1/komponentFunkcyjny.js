023af86c92c31b5c5ca9a5c384db3386040a5717
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-19 06:32:58 +0200

    changes

--- homework/src/ex1/komponentFunkcyjny.js
+++ homework/src/ex1/komponentFunkcyjny.js
@@ -1,9 +1,17 @@
 export const KomponentFunkcyjny = () => {
-    return <article>
-        <div>
-            <h3> 
-                Nauka Reacta 
-            </h3>
-        </div>
-    </article>
+ const naglowek = <h3>Nauka React</h3>
+ const button = <button>Click me</button>
+ const notatki = <textarea placeholder="notatka"></textarea>
+ const obiekt = {
+    name: 'Julia',
+    age: 18,
+    job: 'teacher'
+}
+
+ return(<article>
+    {naglowek}
+    {button}
+    {notatki}
+    {`Name: ${obiekt.name} Age: ${obiekt.age} Job: ${obiekt.job}`}
+ </article>)
 }
