e51ddeed4079cf9806a5118312a0874a3d7f1dc8
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-19 20:49:00 +0200

    import StyleInline in app.js

--- homework/src/App.js
+++ homework/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import "@picocss/pico"
 import { KomponentFunkcyjny } from './ex1/komponentFunkcyjny';
+import { StyleInLine } from './ex1/StyleInline';
 
 function App() {
   return (
     <div className="App">
       <KomponentFunkcyjny/>
+      <StyleInLine/>
     </div>
+    
   );
 }
 
