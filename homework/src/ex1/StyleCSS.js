16a1336023566bf0a58ce6fffe83470ca1046bdb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-20 09:25:50 +0200

    add StyleCSSModules.js

--- homework/src/ex1/StyleCSS.js
+++ homework/src/ex1/StyleCSS.js
@@ -1,13 +1,12 @@
 import React from "react";
-import "./StyleCSS.css"
-
+import "./StyleCSS.css";
 
 export const StyleCSS = () => {
-    const zmienna = "moja-klasa"
-  return <article className={zmienna}>
-    StyleCSS
-    <div className="klasyczna-klasa">
-        div z klasycznie przypisaną klasą
-    </div>
-    </article>;
+  const zmienna = "moja-klasa";
+  return (
+    <article className={zmienna}>
+      StyleCSS
+      <div className="klasyczna-klasa">div z klasycznie przypisaną klasą</div>
+    </article>
+  );
 };
