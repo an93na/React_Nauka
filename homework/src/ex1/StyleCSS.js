cff7340da73b86b74db818c083e247733d580a91
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-20 09:11:49 +0200

    changes StyleCSS.js

--- homework/src/ex1/StyleCSS.js
+++ homework/src/ex1/StyleCSS.js
@@ -1,12 +1,7 @@
-import React from "react"
+import React from "react";
+
+
 
 export const StyleCSS = () => {
-    const zmienna =
-    /*dużo logiki która wybiera jaką klase zaaplikować */ "moja-klasa";
-  return (
-    <article className={zmienna}>
-      StyleCSS
-      <div className="klasa-xd">Div z klasycznie przypisaną klasą</div>
-    </article>
-  );
-}
+  return <article>StyleCSS</article>;
+};
