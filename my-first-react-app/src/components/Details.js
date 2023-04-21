075fabf96c1f4d1e2c8e07f4a359e237db67a549
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-21 19:05:57 +0200

    changes Deatils.js

--- my-first-react-app/src/components/Details.js
+++ my-first-react-app/src/components/Details.js
@@ -3,7 +3,7 @@ import classes from "./allStyles.module.css"
 
 
 export const Deatils = () =>{
-    return <div>
-        
+    return <div className={classes.details}>
+        <p className={classes.przerwa}><b>Victor Crest</b> 26</p><p>London</p>
     </div>
 }
