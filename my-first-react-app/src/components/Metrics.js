b516925d9e50d7640f08414d2c80474d8965b4b4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-21 19:06:27 +0200

    Changes Metric.js

--- my-first-react-app/src/components/Metrics.js
+++ my-first-react-app/src/components/Metrics.js
@@ -1,9 +1,18 @@
 import React from "react";
-import classes from "./allStyles.module.css"
+import classes from "./allStyles.module.css";
 
-
-export const Metric = () =>{
-    return <div>
-
+export const Metric = () => {
+  return (
+    <div style={{display:'flex'}}>
+      <span className={classes.metrics}>
+        <p>80K Followers</p>
+      </span>
+      <span className={classes.metrics}>
+        <p>803K Likes</p>
+      </span>
+      <span className={classes.metrics}>
+        <p>1.4K Photos</p>
+      </span>
     </div>
-}
+  );
+};
