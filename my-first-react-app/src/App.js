7d283907d898c53b0ef2b870e9a25a9d4cbdeec1
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-21 19:06:56 +0200

    changes App.js

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "@picocss/pico";
+import { Profile } from "./components/Profile";
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <article style={{margin:"50px"}}>
+      <Profile/>
+    </article>
   );
 }
 
