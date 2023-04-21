ff7937d8f67faccd8ffa4be6d75f980f896f41a7
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-21 19:48:15 +0200

    changes

--- my-first-react-app/src/components/Details.js
+++ my-first-react-app/src/components/Details.js
@@ -4,6 +4,6 @@ import classes from "./allStyles.module.css"
 
 export const Deatils = () =>{
     return <div className={classes.details} style={{display:"flex", flexDirection: "column"}}>
-        <p className={classes.przerwa}><b>Victor Crest</b> 26</p><p>London</p>
+        <p className={classes.przerwa}><b>Victor Crest</b> 26</p><p className={classes.London}>London</p>
     </div>
 }
--- my-first-react-app/src/components/Metrics.js
+++ my-first-react-app/src/components/Metrics.js
@@ -4,15 +4,15 @@ import classes from "./allStyles.module.css";
 export const Metric = () => {
   return (
     <div style={{display:'flex'}}>
-      <span className={classes.metrics}>
-        <p><b>80K</b> Followers</p>
-      </span>
-      <span className={classes.metrics}>
-        <p><b>803K</b> Likes</p>
-      </span>
-      <span className={classes.metrics}>
-        <p><b>1.4K</b> Photos</p>
-      </span>
+      <div className={classes.metrics}>
+        <p className={classes.liczbaF}><b>80K</b></p><p>Followers</p>
+      </div>
+      <div className={classes.metrics}>
+        <p className={classes.liczbaF}><b>803K</b></p><p>Likes</p>
+      </div>
+      <div className={classes.metrics}>
+        <p className={classes.liczbaF}><b>1.4K</b></p><p> Photos</p>
+      </div>
     </div>
   );
 };
