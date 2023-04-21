be6e88876219bda7df7ec5054fad08001c393fdc
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-21 19:06:40 +0200

    changes Profile.js

--- my-first-react-app/src/components/Profile.js
+++ my-first-react-app/src/components/Profile.js
@@ -1,9 +1,14 @@
 import React from "react";
 import classes from "./allStyles.module.css"
+import { Metric } from "./Metrics";
+import { Logo } from "./Logo";
+import { Deatils } from "./Details";
 
 
 export const Profile = () =>{
-    return <div>
-
+    return <div className={classes.profile}>
+        <Logo/>
+        <Deatils/>
+        <Metric/>
     </div>
 }
