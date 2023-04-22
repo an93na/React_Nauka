264dc066a53634c1245ae8372598e0317ddcb12a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-22 20:37:36 +0200

    changes

--- my-first-react-app/src/components/jakies_testy/Test2.js
+++ my-first-react-app/src/components/jakies_testy/Test2.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 
-
-
 export const Test2 = () => {
-    const [number1, setNumber1] = useState(0);
-    // const [number2, setNumber2] = useState(0);
+  const [number1, setNumber1] = useState(0);
+  // const [number2, setNumber2] = useState(0);
 
-    return (
-        <div>
-            <h4>Policz:</h4>
-            <p>Wartość początkowa: {number1}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h4>Policz:</h4>
+      <p>Wartość początkowa: {number1}</p>
+      <button
+        onClick={() => {
+          const newNumber1 = number1 + 2;
+          setNumber1(newNumber1);
+        }}
+      >
+        +2
+      </button>
+    </div>
+  );
+};
