58d8ec1025f493a7e86ebce5fc81e3c42928a861
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-22 19:39:29 +0200

    changes

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "@picocss/pico";
 import { Profile } from "./components/Profile";
+import { Test1 } from "./components/jakies_testy/Test1";
 
 
 function App() {
   return (
     <article className="App" style={{margin:"50px"}}>
       <Profile/>
+      <Test1/>
     </article>
   );
 }
--- my-first-react-app/src/components/jakies_testy/Test1.js
+++ my-first-react-app/src/components/jakies_testy/Test1.js
@@ -1,4 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 
 
+const ObiektUser= {name: "Pan", lastName: "Kulka", age: 16};
+
+const TODO = {name: 'zrobić pranie', priority: 'High'};
+
+
+export const Test1 = () => {
+    const [user, setUser] = useState(ObiektUser);
+    const [zad, setZad] = useState(TODO);
+
+    return (
+        <article>
+            <h3>Wyświetl Stan</h3>
+            <h4>User:</h4>
+        </article>
+    )
+}
