18d7e26991c5c3d701cef4d08a3d28ac2ff2f136
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-22 20:23:24 +0200

    changes

--- my-first-react-app/src/components/jakies_testy/Test1.js
+++ my-first-react-app/src/components/jakies_testy/Test1.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
+const ObiektUser = { name: "Pan", lastName: "Kulka", age: 16 };
 
-
-const ObiektUser= {name: "Pan", lastName: "Kulka", age: 16};
-
-const TODO = {name: 'zrobić pranie', priority: 'High'};
-
+const TODO = { name: "zrobić pranie", priority: "High" };
 
 export const Test1 = () => {
-    const [user, setUser] = useState(ObiektUser);
-    const [zad, setZad] = useState(TODO);
+  const [user, setUser] = useState(ObiektUser);
+  const [zad, setZad] = useState(TODO);
 
-    return (
-        <div>
-            <h3>Wyświetl Stan</h3>
-            <h4>User:</h4>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <h3>Wyświetl Stan</h3>
+      <h4>User:</h4>
+      <ul style={{ textAlign: "left" }}>
+        <li>
+          Name: <b>{user.name}</b>
+        </li>
+        <li>
+          Last name: <b>{user.lastName}</b>
+        </li>
+        <li>
+          Age: <b>{user.age}</b>
+        </li>
+      </ul>
+    </div>
+  );
+};
