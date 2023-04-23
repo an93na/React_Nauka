d81bccb3dee07b380581fa3b0b019211faaec385
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-23 19:23:08 +0200

    changes

--- my-first-react-app/src/components/jakies_testy/FormularzZTodo.js
+++ my-first-react-app/src/components/jakies_testy/FormularzZTodo.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 
-
-
 export const FormularzZTodo = () => {
-    const [title, setTitle] = useState('');
-    const [priority, setPriority]=useState('');
-    return <div>
-        <h3>Lista rzeczy</h3>
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("");
+
+  const onSubmitPreventDefault = (event) => {
+    event.preventDefault();
+  };
+
+  
+  return (
+    <div>
+      <h3>Lista rzeczy</h3>
+      <form onSubmit={onSubmitPreventDefault}></form>
     </div>
-}
+  );
+};
