5a74d9a2e3ee619026d583772d28825392d8b623
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-23 19:18:34 +0200

    changes

--- my-first-react-app/src/components/jakies_testy/FormularzZTodo.js
+++ my-first-react-app/src/components/jakies_testy/FormularzZTodo.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 
 
 export const FormularzZTodo = () => {
-
+    const [title, setTitle] = useState('');
+    const [priority, setPriority]=useState('');
     return <div>
 
     </div>
