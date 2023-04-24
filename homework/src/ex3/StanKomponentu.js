b0bcfde04c6d4a9674df5cc0b55b41438ea10a35
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-24 19:20:09 +0200

    changes

--- homework/src/ex3/StanKomponentu.js
+++ homework/src/ex3/StanKomponentu.js
@@ -2,15 +2,25 @@ import React, { useState } from "react";
 
 export const StanKomponentu = () => {
   const [state, setState] = useState("test");
-    
-  setTimeout(()=>{
-    setState('koniec Testu')
-  }, 3000)
+  const [name, setName] = useState("Krzyś");
+  const [lastName, setLastName] = useState("Koper");
+  const [age, setAge] = useState(27);
+
+  setTimeout(() => {
+    setState("koniec Testu");
+  }, 3000);
 
   return (
     <article>
       <h4>Stan Komponentu</h4>
       <p>{state}</p>
+      <p>
+        Name: <b>{name}</b>
+      </p>
+      <p>
+        Last name: <b>{lastName}</b>
+      </p>
+      <p>Age: <b>{age}</b></p>
     </article>
   );
 };
