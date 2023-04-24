450fea0ca4ba54fb4b73dc62d07216439aa22e34
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-24 17:55:11 +0200

    add catWeb

--- homework/src/ex3/Asset.js
+++ homework/src/ex3/Asset.js
@@ -4,6 +4,11 @@ import cat2 from "./ddd.png";
 import mapa from './mapa.png'
 
 export const Assets = () => {
+    const catWeb = {
+        name: 'Fluffy',
+        img: 'https://images.pexels.com/photos/1741206/pexels-photo-1741206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    }
+
   return (
     <article>
       <h4>Assets</h4>
@@ -11,6 +16,7 @@ export const Assets = () => {
         <img src={cat} alt="cat" style={{borderRadius: 15, height: 500, width: 500}}/>
         <img src={cat2} alt="cat2" style={{borderRadius: "50%", height: 500, width: 500}}/>
         <img src={mapa} alt="mapa" style={{border: '1px solid red', height: 500, width: 500}}/>
+        <img src={catWeb.img} alt="catWeb" style={{height:500, width:500, borderRadius:"50%"}}/>
       </div>
     </article>
   );
