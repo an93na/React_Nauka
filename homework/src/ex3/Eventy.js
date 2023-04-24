696b2de09c19c5b71dc143e0ed0abe21e2732447
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-24 20:58:32 +0200

    changes

--- homework/src/ex3/Eventy.js
+++ homework/src/ex3/Eventy.js
@@ -1,9 +1,24 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 export const Eventy = () => {
-    return <article>
-        <h3>Events</h3>
+  const [state, setState] = useState([]);
+  const [tekstInputValue, setTekstInputValue] = useState("Siema");
+  const [label, setLabel] = useState("Click me");
+
+  return (
+    <article>
+      <h3>Events</h3>
+      <input type="text" value={tekstInputValue} onChange={(event) => {
+        // console.log(event)
+        setTekstInputValue(event.target.value)}} />
+      <button
+        onClick={() => {
+          console.log("Hello word");
+          setLabel("You Clicked me!");
+        }}
+      >
+        {label}
+      </button>
     </article>
-}
+  );
+};
