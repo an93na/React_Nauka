01db57125950fe6afb280fcd1e3f70ac538b7a94
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-25 19:19:17 +0200

    changes

--- homework/src/ex3/Eventy.js
+++ homework/src/ex3/Eventy.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Counter } from "./components/Counter";
 
 export const Eventy = () => {
   const [state, setState] = useState([]);
@@ -22,6 +23,7 @@ export const Eventy = () => {
       >
         {label}
       </button>
+      <Counter/>
     </article>
   );
 };
--- homework/src/ex3/components/Counter.js
+++ homework/src/ex3/components/Counter.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+
+
+export const Counter = () => {
+    return <div>
+        <p>Counter</p>
+    </div>
+}
