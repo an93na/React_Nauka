4ed1720f9d618b7c0677108d331e7be14b8d3489
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-25 20:49:44 +0200

    changes

--- homework/src/ex3/ZadCom3.js
+++ homework/src/ex3/ZadCom3.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 
 export const ZadCom3 = () => {
+    const[status, setStatus] = useState('');
     return <article>
         <h4>ZadCom3</h4>
+        <p>TextFiled</p>
+        <input type="text" onChange={(event)=>setStatus(event.target.value)}/>
+        <h4>{status}</h4>
     </article>
 }
