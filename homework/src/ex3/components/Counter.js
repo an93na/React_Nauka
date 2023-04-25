4df45dccef1094546dce19e7519bc1e4e1e2e100
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-25 19:39:45 +0200

    changes

--- homework/src/ex3/components/Counter.js
+++ homework/src/ex3/components/Counter.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 export const Counter = () => {
-    const[counter, setCounter]= useState(0)
+  const [counter, setCounter] = useState(0);
   return (
     <div style={{ padding: 15, backgroundColor: "bisque", color: "black" }}>
       Counter
       <p>Counter value: {counter} </p>
-      <button onClick={()=>setCounter(counter+1)}>+1</button>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
+      <button onClick={() => setCounter(counter - 1)}>-1</button>
+      <button onClick={() => setCounter(counter + 5)}>+5</button>
     </div>
   );
 };
