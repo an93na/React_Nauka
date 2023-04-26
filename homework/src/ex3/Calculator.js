72b1b2579c81cff7157bcadc6d45665e8235dc4c
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-26 21:34:43 +0200

    changes

--- homework/src/ex3/Calculator.js
+++ homework/src/ex3/Calculator.js
@@ -4,6 +4,21 @@ export const Calculator = () => {
   const [number1, setNumber1] = useState("");
   const [number2, setNumber2] = useState("");
   const [operation, setOperation] = useState("");
+
+  const rezulat = (n1, n2, op) => {
+    if (op === "+") {
+      return n1 + n2;
+    } else if (op === "-") {
+      return n1 - n2;
+    } else if (op === "*") {
+      return n1 * n2;
+    } else if (op === "-") {
+      return n1 * n2;
+    }
+  };
+
+  const rozwiązanie = rezulat(number1, number2, operation);
+
   return (
     <article>
       <h4>Calculator</h4>
@@ -13,7 +28,10 @@ export const Calculator = () => {
           value={number1}
           onChange={(e) => setNumber1(e.target.value)}
         />
-        <select>
+        <select
+          value={operation}
+          onChange={(e) => setOperation(e.target.value)}
+        >
           <option value="" hidden>
             Wybierz działanie
           </option>
