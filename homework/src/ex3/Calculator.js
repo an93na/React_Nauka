496000dfe4ad24be917f6cb08d3668efa7e3c593
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-26 21:23:37 +0200

    changes

--- homework/src/ex3/Calculator.js
+++ homework/src/ex3/Calculator.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-
-
-export const Calculator = () =>{
-    return <article>
-        
+export const Calculator = () => {
+  return (
+    <article>
+      <h4>Calculator</h4>
+      <div style={{ display: "flex", gap: 10 }}>
+        <input type="text" />
+        <select>
+            <option value='' hidden>Wybierz działanie</option>
+        </select>
+        <input type="text" />
+        <input type="text" readOnly />
+      </div>
     </article>
-}
+  );
+};
