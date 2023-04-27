20a5c9626b01251568529abc1700cfbcb4a5921d
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 20:34:07 +0200

    changes

--- homework/src/ex3/PropsFunkcje.js
+++ homework/src/ex3/PropsFunkcje.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 
 const CounterControler = (props) => {
-  
+  return <div style={{display:'flex', gap:20}}>
+    <button>+1</button>
+    <button>-1</button>
+  </div>
 }; 
 
 const CounterOutput = (props) =>{
-
+return <p>Wartość countera: {}</p>
 }
 
 export const PropsFunkcje = () => {
@@ -13,7 +16,8 @@ export const PropsFunkcje = () => {
   return (
     <article>
       <h4>Propsy - Funkcje</h4>
-      
+      <CounterControler/>
+      <CounterOutput/>
     </article>
   );
 };
