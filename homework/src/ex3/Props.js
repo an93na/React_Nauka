a8f67b1ba670c57dd09d665c2c97a1cb0b98c5da
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 19:56:04 +0200

    changes

--- homework/src/ex3/Props.js
+++ homework/src/ex3/Props.js
@@ -8,8 +8,20 @@ const ComponentWithProps1 = (props) => {
   return <div>Suma dwoch liczb: {number1 + number2}</div>;
 };
 
+const ComponentWithPropsLi = (props) => {
+  return (
+    <li>
+      {props.name} <b>{props.lastName}</b>
+    </li>
+  );
+};
+
 export const Props = () => {
   const [num] = useState(10);
+  const users = [
+    { name: "Paulina", lastName: "Paczka" },
+    { name: "Janusz", lastName: "Jabłko" },
+  ];
   return (
     <article>
       <h4>Propsy</h4>
@@ -17,8 +29,18 @@ export const Props = () => {
       <ComponentWithProps number1={4} number2={num} />
       <ComponentWithProps number1={4} number2={5} />
       <ComponentWithProps number2={5} />
-      <ComponentWithProps1/>
-      <ComponentWithProps1 number1={12}/>
+      <ComponentWithProps1 />
+      <ComponentWithProps1 number1={12} />
+      <ul>
+        <ComponentWithPropsLi
+          name={users[0].name}
+          lastName={users[0].lastName}
+        />
+        <ComponentWithPropsLi
+          name={users[1].name}
+          lastName={users[1].lastName}
+        />
+      </ul>
     </article>
   );
 };
