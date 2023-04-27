6429d776db620d1319dbb7c75f301a67a93dbbcb
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 17:16:19 +0200

    changes

--- homework/src/ex3/Form.js
+++ homework/src/ex3/Form.js
@@ -1,9 +1,17 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 export const Form = () => {
-    return <article>
-        <h4>Form</h4>
+  const [task, setTask] = useState();
+  const [priority, setPriority] = useState();
+
+  return (
+    <article>
+      <h4>Form</h4>
+      <form>
+        <input type="text" name="" id="" placeholder="Task title" />
+        <input type="text" name="" id="" placeholder="Select priority" />
+        <button>Submit</button>
+      </form>
     </article>
-}
+  );
+};
