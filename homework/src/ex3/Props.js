88dc0629b7ae1b83ec98dc177d7d72d208eb2599
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 19:47:34 +0200

    changes

--- homework/src/ex3/Props.js
+++ homework/src/ex3/Props.js
@@ -1,16 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 
 const ComponentWithProps = (props) => {
-    return <div>
-        Suma dwoch liczb: {props.number1+props.number2}
-    </div>
-}
+  return <div>Suma dwoch liczb: {props.number1 + props.number2}</div>;
+};
+const ComponentWithProps1 = (props) => {
+  const { number1 = 10, number2 = 10 } = props;
+  return <div>Suma dwoch liczb: {number1 + number2}</div>;
+};
 
-
-export const Props = () =>{
-    return <article>
-        <h4>Propsy</h4>
-        <ComponentWithProps number1={20} number2={10}/>
+export const Props = () => {
+  const [num] = useState(10);
+  return (
+    <article>
+      <h4>Propsy</h4>
+      <ComponentWithProps number1={20} number2={10} />
+      <ComponentWithProps number1={4} number2={num} />
+      <ComponentWithProps number1={4} number2={5} />
+      <ComponentWithProps number2={5} />
+      <ComponentWithProps1/>
+      <ComponentWithProps1 number1={12}/>
     </article>
-}
+  );
+};
