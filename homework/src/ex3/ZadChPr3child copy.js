665f041009b342169522bebbd0748a30fc1e959b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 22:13:40 +0200

    changes

---
+++ homework/src/ex3/ZadChPr3child copy.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Child4 = (props) => {
+
+};
---
+++ homework/src/ex3/ZadChlPr4.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Parent4 = () => {
+  return (
+    <article>
+      <h5>Parent4</h5>
+      
+    </article>
+  );
+};
