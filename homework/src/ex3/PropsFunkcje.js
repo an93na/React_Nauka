593c73cf904d2bc8ef8113bbe8cee6c1dd018813
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-27 20:46:43 +0200

    changes

--- homework/src/ex3/PropsFunkcje.js
+++ homework/src/ex3/PropsFunkcje.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 
 const CounterControler = (props) => {
-  return <div style={{display:'flex', gap:20}}>
-    <button>+1</button>
-    <button>-1</button>
-  </div>
-}; 
+  return (
+    <div style={{ display: "flex", gap: 20 }}>
+      <button onClick={props.funkcjaInkrementujaca}>+1</button>
+      <button onClick={props.funkcjaDekrementujaca}>-1</button>
+    </div>
+  );
+};
 
-const CounterOutput = (props) =>{
-return <p>Wartość countera: {}</p>
-}
+const CounterOutput = (props) => {
+  return <p>Wartość countera: {props.output}</p>;
+};
 
 export const PropsFunkcje = () => {
- 
+  const [counter, setCounter] = useState(0);
+
+  const handleDecreaseClick = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <article>
       <h4>Propsy - Funkcje</h4>
-      <CounterControler/>
-      <CounterOutput/>
+      <button onClick={() => setCounter(counter + 5)}>+5</button>
+      <CounterControler
+        funkcjaInkrementujaca={() => setCounter(counter + 1)}
+        funkcjaDekrementujaca={handleDecreaseClick}
+      />
+      <CounterOutput output={counter} />
     </article>
   );
 };
