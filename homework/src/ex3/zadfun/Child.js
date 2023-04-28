b32ebe78c025c69c26b69d3fd0a6ca1a3ebec1bf
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-28 19:24:33 +0200

    add file Child.js

---
+++ homework/src/ex3/zadfun/Child.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export const ChildDoGrand = (props) => {
+    return <div style={{ margin: 10, padding: 10, border: "1px solid green" }}>
+        Child
+    </div>
+}
--- homework/src/ex3/zadfun/Parent.js
+++ homework/src/ex3/zadfun/Parent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { ChildDoGrand } from "./Child";
 
 
 
-export const ParentdoGrand = () =>{
-    return <div>
+export const ParentdoGrand = (props) =>{
+    return <div style={{ margin: 10, padding: 10, border: "1px solid blue" }}>
         Parent
+        <ChildDoGrand name={props.name} lastName= {props.lastName} jakasFunkcja={props.jakasFunkcja}/>
     </div>
 }
