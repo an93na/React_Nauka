6c34f8601a641f7d4b98d547bc9bac751064b494
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-28 19:37:15 +0200

    add GrandChild.js

--- homework/src/ex3/zadfun/Child.js
+++ homework/src/ex3/zadfun/Child.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-
+import { GrandChild } from "./GrandChild";
 
 export const ChildDoGrand = (props) => {
-    return <div style={{ margin: 10, padding: 10, border: "1px solid green" }}>
-        Child
+  return (
+    <div style={{ margin: 10, padding: 10, border: "1px solid green" }}>
+      Child
+      <GrandChild name={props.name} lastName = {props.lastName} age = {props.age} jakasFunkcja = {props.jakasFunkcja}/>
     </div>
-}
+  );
+};
---
+++ homework/src/ex3/zadfun/GrandChild.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export const GrandChild = (props) =>{
+    return <div style={{ margin: 10, padding: 10, border: "1px solid red" }}>
+        GrandChild 
+    </div>
+}
