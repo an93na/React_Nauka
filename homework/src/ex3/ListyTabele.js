ba0b1e7a4d942e8963b77c8e6c39f076b1cc18d4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-28 10:01:55 +0200

    chamges

--- homework/src/ex3/ListyTabele.js
+++ homework/src/ex3/ListyTabele.js
@@ -1,40 +1,56 @@
 import React, { useState } from "react";
 
-const TASKS =
-    [
-        {
-          "title": "zrobić pranie",
-          "priority": "High"
-        },
-        {
-          "title": "praca domowa",
-          "priority": "Medium"
-        },
-        {
-          "title": "Zrobić zakupy",
-          "priority": "Medium"
-        }
-      ]
+const TASKS = [
+  {
+    title: "zrobić pranie",
+    priority: "High",
+    id: "1",
+  },
+  {
+    title: "praca domowa",
+    priority: "Medium",
+    id: "2",
+  },
+  {
+    title: "Zrobić zakupy",
+    priority: "Low",
+    id: "3",
+  },
+];
 
 export const ListyTabele = () => {
-    const [tasks, setTasks] = useState(TASKS);
-    const tablicaStringow = ['test1', 'test2', 'test3'];
-    
-    return <article>
-        <h4>ListyTabele</h4>
-        <ul>
-            {tablicaStringow.map( (element) => {
-                return <li key={element}>{element}</li>
-            })}
-        </ul>
-        <table>
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Priority</th>
-                    <th></th>
-                </tr>
-            </thead>
-        </table>
+  const [tasks, setTasks] = useState(TASKS);
+  const tablicaStringow = ["test1", "test2", "test3"];
+
+  return (
+    <article>
+      <h4>ListyTabele</h4>
+      <ul>
+        {tablicaStringow.map((element) => {
+          return <li key={element}>{element}</li>;
+        })}
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Priority</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task) => {
+            return (
+              <tr key={task.id}>
+                <th>{task.id}</th>
+                <th>{task.title}</th>
+                <th>{task.priority}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </article>
-}
+  );
+};
