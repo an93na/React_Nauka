08610210ad5f5485f1f607e1ea20b2e39419f8d4
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-28 20:06:14 +0200

    changes

--- homework/src/ex3/zadfun/GrandChild.js
+++ homework/src/ex3/zadfun/GrandChild.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
+// export const GrandChild = (props) => {
+//   return (
+//     <div style={{ margin: 10, padding: 10, border: "1px solid red" }}>
+//       GrandChild
+//       <ul>
+//         <li>Name: {props.name}</li>
+//         <li>Last Name: {props.lastName}</li>
+//         <li>Age: {props.age}</li>
+//       </ul>
+//       <button onClick={props.jakasFunkcja}>Click me</button>
+//     </div>
+//   );
+// };
 
+export const GrandChild = (props) => {
+  const [showData, setShowData] = useState(false);
 
-export const GrandChild = (props) =>{
-    return <div style={{ margin: 10, padding: 10, border: "1px solid red" }}>
-        GrandChild 
+  const handleClick = () => {
+    setShowData(true);
+  };
+
+  return (
+    <div style={{ margin: 10, padding: 10, border: "1px solid red" }}>
+      GrandChild
+      <button onClick={handleClick}>Click</button>
+      {showData && (
         <ul>
-            <li>Name: {props.name}</li>
-            <li>Last Name: {props.lastName}</li>
-            <li>Age: {props.age}</li>
+          <li>Name: {props.name}</li>
+          <li>Last Name: {props.lastName}</li>
+          <li>Age: {props.age}</li>
         </ul>
-        <button onClick={props.jakasFunkcja}>Click me</button>
+      )}
+      <button onClick={props.jakasFunkcja}>Click me</button>
     </div>
-}
+  );
+};
