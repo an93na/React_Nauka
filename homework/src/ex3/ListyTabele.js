24863f787f5d76b36c09bbe518aa9cb637885fda
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-28 09:37:26 +0200

    changes

--- homework/src/ex3/ListyTabele.js
+++ homework/src/ex3/ListyTabele.js
@@ -3,7 +3,14 @@ import React from "react";
 
 
 export const ListyTabele = () => {
+    const tablicaStringow = ['test1', 'test2', 'test3'];
+    
     return <article>
         <h4>ListyTabele</h4>
+        <ul>
+            {tablicaStringow.map( (element) => {
+                return <li key={element}>{element}</li>
+            })}
+        </ul>
     </article>
 }
