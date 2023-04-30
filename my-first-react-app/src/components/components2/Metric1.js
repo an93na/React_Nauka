eaad1af16c5e40ebbcf41d5fa3060b82d96a2e94
Anna Giermakowska <an.giermakowska@gmail.com>
2023-04-30 20:26:45 +0200

    changes

--- my-first-react-app/src/components/components2/Metric1.js
+++ my-first-react-app/src/components/components2/Metric1.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import classes from "./Metric.module.css";
 
-export const Metric1 = () => {
-  const [name, setName] = useState("Work");
-  const [label, setLabel] = useState("32hrs");
-  const [prevweek, setPrevweek] = useState("Last Week - 36hrs");
+export const Metric1 = (props) => {
+  
   return (
     <div className={classes.outerContainer}>
       <div className={classes.innerContainer}>
         <p className={classes.label}>
-          {name} <span>•••</span>
+          {props.name} <span>•••</span>
         </p>
-        <p className={classes.output}>{label}</p>
-        <p className={classes.lastWeekOutput}>{prevweek}</p>
+        <p className={classes.output}>{props.label}</p>
+        <p className={classes.lastWeekOutput}>{props.prevweek}</p>
       </div>
     </div>
   );
--- my-first-react-app/src/components/components2/MetricResult.js
+++ my-first-react-app/src/components/components2/MetricResult.js
@@ -3,12 +3,14 @@ import { Metric1 } from "./Metric1";
 import { Metric2 } from "./Metric2";
 import { Metric3 } from "./Metric3";
 
-export const MetricsApp = (props) => {
-
+export const MetricsApp = () => {
+  const [name, setName] = useState("Work");
+  const [label, setLabel] = useState("32hrs");
+  const [prevweek, setPrevweek] = useState("Last Week - 36hrs");
   return (
     <article>
       <div style={{ display: "flex", gap: 20 }}>
-        <Metric1 />
+        <Metric1 name={name} label={label} prevweek = {prevweek}/>
         <Metric2 />
         <Metric3 />
       </div>
