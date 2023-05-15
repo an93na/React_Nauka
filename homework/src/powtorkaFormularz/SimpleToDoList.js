65dcc0fb4cc41eb63e6cc10b560a74c23303395a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 09:54:32 +0200

    changes

--- homework/src/powtorkaFormularz/SimpleToDoList.js
+++ homework/src/powtorkaFormularz/SimpleToDoList.js
@@ -8,17 +8,34 @@ export const SimpleTodoList = () => {
     // undefined,
     "Pouczyć się CSSa",
   ]);
-  const [todoTitle, setTodoTitle] = useState('czesc');
+  const [todoTitle, setTodoTitle] = useState("czesc");
   return (
     <article>
       <h4>SimpleTodoList</h4>
-      <form action="" onSubmit={(e) => {
-        e.preventDefault();
-      }} >
-        <input type="text" name="" id="" value={todoTitle} onChange={(event) => setTodoTitle(event.target.value)}/>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (todoTitle !== "") {
+            setTask((prevState) => [...prevState, todoTitle]);
+            setTodoTitle("");
+          }
+        }}
+      >
+        <input
+          type="text"
+          name=""
+          id=""
+          value={todoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+        />
         <button type="submit">Submit</button>
       </form>
-      <ul></ul>
+      <ul>
+        {tasks.map((task) => {
+          return <li key={task}>{task}</li>;
+        })}
+      </ul>
     </article>
   );
 };
