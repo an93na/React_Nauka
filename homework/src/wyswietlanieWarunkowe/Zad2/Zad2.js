9c1c983143375df9e2d1bb7040ffb62c2996a3fd
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 13:18:22 +0200

    changes

--- homework/src/wyswietlanieWarunkowe/Zad2/Zad2.js
+++ homework/src/wyswietlanieWarunkowe/Zad2/Zad2.js
@@ -1,9 +1,31 @@
-import React from "react";
-
-
+import React, { useState } from "react";
+import DOG from "./assets/dog.jpg"
+import CAT from "./assets/cat.webp"
+import BOTH from "./assets/both.jpeg"
 
 export const Zad2 = () => {
-    return <article>
-        
+    const [animal, setAnimal] = useState('')
+
+    const handleChange = (event) => {
+       setAnimal(event.target.value)
+    }
+  return (
+    <article>
+      <p>Are you cat or dog person?</p>
+      <div>
+        <label>
+          <input value="dog" type="radio" name="dogOrCat" checked={animal === 'dog'} onChange={handleChange} /> Dog
+        </label>
+        <label>
+          <input value="cat" type="radio" name="dogOrCat" checked={animal === 'cat'} onChange={handleChange} /> Cat
+        </label>
+        <label>
+          <input value="both" type="radio" name="dogOrCat" checked={animal === 'both'} onChange={handleChange} /> Both
+        </label>
+      </div>
+      {animal==='dog' && <img width="300" src={DOG} alt="dog"/>} 
+      {animal==='cat' && <img width="300" src={CAT} alt="cat"/>} 
+      {animal==='both' && <img width="300" src={BOTH} alt="both"/>} 
     </article>
-}
+  );
+};
