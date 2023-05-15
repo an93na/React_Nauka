169047031a39d462b9e717bc09dafb24b9783810
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 13:43:23 +0200

    changes

--- homework/src/wyswietlanieWarunkowe/Zad2/Zad2.js
+++ homework/src/wyswietlanieWarunkowe/Zad2/Zad2.js
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
-import DOG from "./assets/dog.jpg"
-import CAT from "./assets/cat.webp"
-import BOTH from "./assets/both.jpeg"
+import DOG from "./assets/dog.jpg";
+import CAT from "./assets/cat.webp";
+import BOTH from "./assets/both.jpeg";
 
 export const Zad2 = () => {
-    const [animal, setAnimal] = useState('')
+  const [animal, setAnimal] = useState("");
 
-    const handleChange = (event) => {
-       setAnimal(event.target.value)
-    }
+  const handleChange = (event) => {
+    setAnimal(event.target.value);
+  };
   return (
     <article>
       <p>Are you cat or dog person?</p>
       <div>
         <label>
-          <input value="dog" type="radio" name="dogOrCat" checked={animal === 'dog'} onChange={handleChange} /> Dog
+          <input
+            value="dog"
+            type="radio"
+            name="dogOrCat"
+            checked={animal === "dog"}
+            onChange={handleChange}
+          />{" "}
+          Dog
         </label>
         <label>
-          <input value="cat" type="radio" name="dogOrCat" checked={animal === 'cat'} onChange={handleChange} /> Cat
+          <input
+            value="cat"
+            type="radio"
+            name="dogOrCat"
+            checked={animal === "cat"}
+            onChange={handleChange}
+          />{" "}
+          Cat
         </label>
         <label>
-          <input value="both" type="radio" name="dogOrCat" checked={animal === 'both'} onChange={handleChange} /> Both
+          <input
+            value="both"
+            type="radio"
+            name="dogOrCat"
+            checked={animal === "both"}
+            onChange={handleChange}
+          />{" "}
+          Both
         </label>
       </div>
-      {animal==='dog' && <img width="300" src={DOG} alt="dog"/>} 
-      {animal==='cat' && <img width="300" src={CAT} alt="cat"/>} 
-      {animal==='both' && <img width="300" src={BOTH} alt="both"/>} 
+      {(animal === "dog" || animal === "both") && (
+        <img width="300" src={DOG} alt="dog" />
+      )}
+      {(animal === "cat" || animal === "both") && (
+        <img width="300" src={CAT} alt="cat" />
+      )}
+      {animal === "both" && <img width="300" src={BOTH} alt="both" />}
     </article>
   );
 };
