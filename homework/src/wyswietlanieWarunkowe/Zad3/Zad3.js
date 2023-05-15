abe5bf0cd5251943ac146c4db10b9ec6999bafd2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 14:08:59 +0200

    changes

--- homework/src/wyswietlanieWarunkowe/Zad3/Zad3.js
+++ homework/src/wyswietlanieWarunkowe/Zad3/Zad3.js
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { FormWarun } from "./components/Form";
+import { Home } from "./components/Home";
+import { LitsWar } from "./components/List";
 
-
+const style = {
+  margin: 20,
+  padding: 20,
+  border: "2px solid blue",
+  display: "flex",
+  borderRadius: 5,
+  gap: 50,
+};
 
 export const Zad3 = () => {
-    return <article>
-        
+  const [isChecked, setIsChecked] = useState("Home");
+
+  const handleChange = (event) => {
+    setIsChecked(event.target.value);
+  };
+  return (
+    <article>
+      <div style={style}>
+        <label>
+          <input
+            type="radio"
+            value="Form"
+            name="kom"
+            id=""
+            checked={isChecked === "Form"}
+            onChange={handleChange}
+          />
+          Form
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Home"
+            name="kom"
+            id=""
+            checked={isChecked === "Home"}
+            onChange={handleChange}
+          />
+          Home
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="List"
+            name="kom"
+            id=""
+            checked={isChecked === "List"}
+            onChange={handleChange}
+          />
+          List
+        </label>
+      </div>
+
+      {isChecked === "Form" && <FormWarun />}
+      {isChecked === "Home" && <Home />}
+      {isChecked === "List" && <LitsWar />}
     </article>
-}
+  );
+};
--- homework/src/wyswietlanieWarunkowe/Zad3/components/Home.js
+++ homework/src/wyswietlanieWarunkowe/Zad3/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 
 export const Home = () => {
-  <div>
-    <h4>Home</h4>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-      explicabo labore odio, praesentium veritatis cupiditate veniam nam odit
-      error aperiam illum alias, sequi facilis quis atque perferendis earum?
-      Unde, perferendis! Eveniet ratione aspernatur aliquid reprehenderit
-      minima, ad et excepturi esse itaque quod dolorum veniam facilis eligendi
-      dicta officia illum quaerat saepe impedit at expedita natus doloribus
-      iure? Fugit, vero nam? Ipsa accusantium hic deleniti autem exercitationem
-      eligendi fugit quae iste? Deleniti minus aliquam eligendi sequi. Nisi
-      maxime, harum corporis praesentium inventore porro illo voluptatibus iste
-      eligendi tempora dicta soluta iure!
-    </p>
-  </div>;
+  return (
+    <div>
+      <h4>Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        explicabo labore odio, praesentium veritatis cupiditate veniam nam odit
+        error aperiam illum alias, sequi facilis quis atque perferendis earum?
+        Unde, perferendis! Eveniet ratione aspernatur aliquid reprehenderit
+        minima, ad et excepturi esse itaque quod dolorum veniam facilis eligendi
+        dicta officia illum quaerat saepe impedit at expedita natus doloribus
+        iure? Fugit, vero nam? Ipsa accusantium hic deleniti autem
+        exercitationem eligendi fugit quae iste? Deleniti minus aliquam eligendi
+        sequi. Nisi maxime, harum corporis praesentium inventore porro illo
+        voluptatibus iste eligendi tempora dicta soluta iure!
+      </p>
+    </div>
+  );
 };
