0f8c6585741b3c1c83c102b575ded811e66c80c3
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 18:27:57 +0200

    changes

--- homework/src/cyklZyciaKomponentu/CyklZyciaKomp.js
+++ homework/src/cyklZyciaKomponentu/CyklZyciaKomp.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { ListZ } from "./components/ListZ";
 
 
 
 export  const CyklZyciaKomponentu = () => {
+    const [check, setCheck] = useState(false)
     return <article>
-        <ListZ />
+        <input type="checkbox" name="" id="" checked={check} onChange={(e)=> setCheck((prev) => !prev)}/>
+        {check === true && <ListZ />}
     </article>
 }
--- homework/src/cyklZyciaKomponentu/components/ListZ.js
+++ homework/src/cyklZyciaKomponentu/components/ListZ.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
 
 export const ListZ = () => {
-    return <div>
+    useEffect(() => {
+        console.log('komponent się zamontował');
         
+        fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      return () => {
+        console.log('komponent się odmontował')
+      }
+    }, [])
+    return <div>
+
     </div>
 }
