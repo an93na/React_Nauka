9f7838f6521763023566b4a23b684a4bc9889cf2
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 12:48:26 +0200

    changes

--- homework/src/wyswietlanieWarunkowe/Zad1/Zad1.js
+++ homework/src/wyswietlanieWarunkowe/Zad1/Zad1.js
@@ -1,14 +1,28 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 export const Zad1 = () => {
-
-    return <article>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem sapiente atque expedita quaerat sed quo, quis saepe tempora, aliquid molestiae officiis non blanditiis, provident temporibus? Nesciunt quam ratione facere natus!
-        Quos, quod? Ullam commodi nam, ipsam officia autem eveniet cumque repudiandae, soluta dolore molestiae sint, dicta nulla dolorum qui natus tempore optio quo. Mollitia amet eius blanditiis suscipit neque ex.
-        Dolorum, eum quae. Ipsam, obcaecati. Nam rem reprehenderit assumenda, vitae nisi accusamus saepe iste quidem recusandae voluptatem at expedita adipisci, nemo voluptate nihil itaque possimus doloribus quod harum, a molestias?</p>
+  const [read, setRead] = useState(false);
+  return (
+    <article>
+      {read ? (
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Exercitationem sapiente atque expedita quaerat sed quo, quis saepe
+          tempora, aliquid molestiae officiis non blanditiis, provident
+          temporibus? Nesciunt quam ratione facere natus! Quos, quod? Ullam
+          commodi nam, ipsam officia autem eveniet cumque repudiandae, soluta
+          dolore molestiae sint, dicta nulla dolorum qui natus tempore optio
+          quo. Mollitia amet eius blanditiis suscipit neque ex. Dolorum, eum
+          quae. Ipsam, obcaecati. Nam rem reprehenderit assumenda, vitae nisi
+          accusamus saepe iste quidem recusandae voluptatem at expedita
+          adipisci, nemo voluptate nihil itaque possimus doloribus quod harum, a
+          molestias?
+        </p>
+      ) : (
         <p>Lorem ipsum dolor sit amet.</p>
-        <button style={{width:'fit-content'}}>Read </button>
+      )}
+
+      <button style={{ width: "fit-content" }} onClick={() => setRead((prevState) => !prevState)}> {read? 'Read less' : 'Read more'} </button>
     </article>
-}
+  );
+};
