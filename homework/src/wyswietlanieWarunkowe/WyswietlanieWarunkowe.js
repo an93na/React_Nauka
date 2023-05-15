b4f74269c7f6b7a925abc2f6f26d8fa7959cd1ad
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-15 11:00:03 +0200

    changes

--- homework/src/wyswietlanieWarunkowe/WyswietlanieWarunkowe.js
+++ homework/src/wyswietlanieWarunkowe/WyswietlanieWarunkowe.js
@@ -1,9 +1,19 @@
-import React from "react";
-
-
+import React, { useState } from "react";
 
 export const WyswietlanieWarunkowe = () => {
-    return <article>
-        <h4>Wyświetlanie Warunkowe</h4>
+  const [isChecked, setIsChecked] = useState(false);
+
+  return (
+    <article>
+      <h4>Wyświetlanie Warunkowe</h4>
+      <ul>
+        <li>&&</li>
+        <li>Ternary</li>
+        <li>if</li>
+      </ul>
+      wyświetl paragraf <input type="checkbox" name="" id="" checked={isChecked} onChange={(event)=> setIsChecked(prevState => !prevState)}/>
+      
+      {isChecked && <p>Lorem ipsum dolor sit amet.</p>}
     </article>
-}
+  );
+};
