5a5c0e98d07e3910ba079c7aa757055cbea209f9
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-16 20:42:52 +0200

    changes

--- homework/src/cyklZyciaKomponentu/components/ListZ4.js
+++ homework/src/cyklZyciaKomponentu/components/ListZ4.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
 
 export const ListZ4 = () => {
-    return <div>
-        
-    </div>
+    const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Completed</th>
+          <th>actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map((todo) => {
+          return (
+            <tr key={todo.id}>
+              <td>{todo.title}</td>
+              <td>{todo.completed ? "Yes" : "No"}</td>
+              <td>
+                <button>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
+
+
+// fetch("http://localhost:3010/todos")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setTodos(data);
+//       });
