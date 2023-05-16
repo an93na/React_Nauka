fe75dbbb8a9dbd37d6597e5b19ec512c2d63c3fe
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-16 20:30:31 +0200

    changes

--- homework/src/cyklZyciaKomponentu/CyklZyciaKomp.js
+++ homework/src/cyklZyciaKomponentu/CyklZyciaKomp.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ListZ } from "./components/ListZ";
 import { ListZ2 } from "./components/ListZ2";
 import { ListZ3 } from "./components/ListZ3";
+import { ListZ4 } from "./components/ListZ4";
 
 
 
@@ -9,6 +10,7 @@ export  const CyklZyciaKomponentu = () => {
     const [check, setCheck] = useState(false)
     const [check2, setCheck2] = useState(false)
     const [check3, setCheck3] = useState(false)
+    const [check4, setCheck4] = useState(false)
     return <article>
         <label>
             <input type="checkbox" name="" id="" checked={check} onChange={()=> setCheck((prev) => !prev)}/>
@@ -23,8 +25,13 @@ export  const CyklZyciaKomponentu = () => {
         <label>
             <input type="checkbox" name="" id="" checked={check3} onChange={()=> setCheck3((prev) => !prev)}/>
         Pokaż dane3
-        </label>
+        </label> 
         {check3 === true && <ListZ3 />}
+        <label>
+            <input type="checkbox" name="" id="" checked={check4} onChange={()=> setCheck4((prev) => !prev)}/>
+        Pokaż dane4
+        </label>
+        {check4 === true && <ListZ4 />}
 
     </article>
 }
---
+++ homework/src/cyklZyciaKomponentu/components/ListZ4.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+export const ListZ4 = () => {
+    return <div>
+        
+    </div>
+}
