7f36d4cb22dc5484935b26afe1fc7a920743948f
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-17 20:01:46 +0200

    changes

--- TodoForm/todolist/src/components/Form.js
+++ TodoForm/todolist/src/components/Form.js
@@ -1,13 +1,36 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 export const Form = () => {
-    const [title, setTitle] = useState('');
-    const [priority, setPriority] = useState('')
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState("");
   return (
     <article>
-      <form action="">
-        <input type="text" name="" id="" value={title} placeholder="Todo title" onChange={(event) => setTitle(event.target.value)}/>
-        <select name="" id="">
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          const newTodo = {
+            id: Math.random(),
+            status: false,
+            title,
+            priority,
+          };
+        }}
+      >
+        <input
+          type="text"
+          name=""
+          id=""
+          value={title}
+          placeholder="Todo title"
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <select
+          name=""
+          id=""
+          value={priority}
+          onChange={(event) => setPriority(event.target.value)}
+        >
           <option value="" hidden>
             Select priority
           </option>
