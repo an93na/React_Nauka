a47eabac0fce229c4f6cc0dc56124d88acac3987
Anna Giermakowska <an.giermakowska@gmail.com>
2023-05-18 19:16:33 +0200

    changes

--- TodoForm/todolist/src/App.js
+++ TodoForm/todolist/src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form } from "./components/Form";
 import { TodoList } from "./components/TodoList";
 import { EditModal } from "./components/EditModal";
+import { Filters } from "./components/Filters";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -11,6 +12,7 @@ function App() {
     <div className="container">
       <article>Hello Word</article>
       <Form setTodos={setTodos}/>
+      <Filters />
       <TodoList todos={todos} setTodos={setTodos} setTodoToEdit={setTodoToEdit}/>
       {!!todoToEdit && <EditModal setTodoToEdit={setTodoToEdit} todoToEdit={todoToEdit} setTodos={setTodos}/>}
     </div>
--- TodoForm/todolist/src/components/Filters.js
+++ TodoForm/todolist/src/components/Filters.js
@@ -3,28 +3,29 @@ import React from "react";
 export const Filters = () => {
   return (
     <article>
-      <label htmlFor="Title">
+      <div style={{ display: "flex", gap: 300, padding: 10 }}>
+        <label htmlFor="Title">Title</label>
+        <label htmlFor="Select1">Priority</label>
+        <label htmlFor="Select2">Status</label>{" "}
+      </div>
+      <div style={{ display: "flex", gap: 20, padding: 10 }}>
         <input
           type="text"
           name=""
           id="Title"
           placeholder="Enter title you search"
         />
-      </label>
-      <label htmlFor="Select1">
         <select name="" id="Select1">
           <option value="" hidden>
             All
           </option>
         </select>
-      </label>
-      <label htmlFor="Select2">
         <select name="" id="Select2">
           <option value="" hidden>
             All
           </option>
         </select>
-      </label>
+      </div>
     </article>
   );
 };
