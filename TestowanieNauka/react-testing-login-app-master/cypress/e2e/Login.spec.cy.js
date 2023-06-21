576a9f7595f2dcd3b35159efcb62e08f015b513a
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-21 12:04:39 +0200

    changes

--- TestowanieNauka/react-testing-login-app-master/cypress/e2e/Login.spec.cy.js
+++ TestowanieNauka/react-testing-login-app-master/cypress/e2e/Login.spec.cy.js
@@ -5,5 +5,6 @@ describe("Login E2E Form", () => {
   it("display success message on valid email and chceck existing eamil and password input", () => {
     cy.get('input[type="email"]').type("test@example.pl");
     cy.get('input[type="password"]').type("cookies");
-  });
+    cy.get('button[data-testid="submit"]').click();
+});
 });
