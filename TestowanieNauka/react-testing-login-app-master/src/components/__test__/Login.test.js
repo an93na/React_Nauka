4e777ec41274fa396741d94f71b76a897d0590b6
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-22 20:05:26 +0200

    changes

--- TestowanieNauka/react-testing-login-app-master/src/components/__test__/Login.test.js
+++ TestowanieNauka/react-testing-login-app-master/src/components/__test__/Login.test.js
@@ -1,9 +1,14 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "../Login";
 
 describe("Login Component", () => {
   test("render Login without error", () => {
     render(<Login />);
   });
+  test("renders exist form elements", () => {
+    render(<Login/>)
+    const eamilInput = screen.getByLabelText("email")
+    const passwordInput = screen.getByLabelText("password")
+  })
 });
