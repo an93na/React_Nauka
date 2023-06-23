a1ea807f068d7fd137473fb94df13cd37d002357
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-23 19:40:04 +0200

    changes

--- TestowanieNauka/react-testing-login-app-master/src/components/__test__/Login.test.js
+++ TestowanieNauka/react-testing-login-app-master/src/components/__test__/Login.test.js
@@ -11,11 +11,25 @@ describe("Login Component", () => {
     const emailInput = screen.getByLabelText("Email");
     const passwordInput = screen.getByLabelText("Password");
     const submittButton = screen.getByTestId("submit");
-        const resetButton = screen.getByTestId("reset");
+    const resetButton = screen.getByTestId("reset");
 
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(submittButton).toBeInTheDocument();
     expect(resetButton).toBeInTheDocument();
+  });
+});
+
+describe("validateEmail Function", () => {
+  test("return true for a vaild email", () => {
+    const validEmails = [
+      "test@gmail.cm",
+      "nowyEmail@onet.pll",
+      "john.dog@gmail.ccom",
+      "user1234@test.co",
+    ];
+    validEmails.forEach((email) => {
+      expect(validEmails(email)).toBe(true);
+    });
   });
 });
