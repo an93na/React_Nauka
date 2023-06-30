31e74b6fe2c6b8a352ade9e01a885d8b8fa4c09b
Anna Giermakowska <an.giermakowska@gmail.com>
2023-06-30 19:30:08 +0200

    changes

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -3,3 +3,6 @@ const resultDay = day.addEventListener("input", () => console.log(day.value));
 
 const month = document.querySelector('#month')
 const resultMonth = month.addEventListener("input", () => console.log(month.value))
+
+const year = document.querySelector('#year')
+const resultYear = month.addEventListener("input", () => console.log(month.value))
