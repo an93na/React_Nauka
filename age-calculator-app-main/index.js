8703b0f585217170043ef6b6f953c4b1feb5ca77
Anna Giermakowska <an.giermakowska@gmail.com>
2023-07-01 18:45:23 +0200

    changes

--- age-calculator-app-main/index.js
+++ age-calculator-app-main/index.js
@@ -1,14 +1,11 @@
-const lista = document.querySelector('#lista');
+const lista = document.querySelector("#lista");
 const day = document.querySelector("#day");
 const resultDay = day.addEventListener("input", () => {
-// console.log(day.value)
-const li1 = document.createElement('li');
-li1.innerText = `${day.value} day`
-const resultUl1 = lista.appendChild(li1);}
-
-
-
-);
+  // console.log(day.value)
+  const li1 = document.createElement("li");
+  li1.innerText = `${day.value} day`;
+  lista.appendChild(li1);
+});
 
 const month = document.querySelector("#month");
 const resultMonth = month.addEventListener("input", () =>
